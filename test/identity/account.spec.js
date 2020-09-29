062cf75edae93278b0be64e39e1fe4587e6cd757
sergewilfried <sergewilfried@outlook.fr>
2020-09-29 03:23:43 -0400

    fixed unit tests

--- test/identity/account.spec.js
+++ test/identity/account.spec.js
@@ -2,10 +2,12 @@ const expect = require('chai').expect;
 const { getAccount } = require('../../src/identity/account');
 const { getContractKit } = require('../../src/identity/account');
 
-const account = getAccount();
 
 describe('getContractKit', () => {
+    const account = getAccount();
+
     it('gets the contractKit for the account correctly', () => {
+
         expect(account.address).to.equal(getContractKit(account).defaultAccount);
     });
 });
